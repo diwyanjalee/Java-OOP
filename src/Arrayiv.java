0aed773183467a65949cb21183b0dc239ecc5832
diwyanjalee <charithadiwyanjalee@gmail.com>
2021-08-14 14:44:28 +0530

    array update

---
+++ src/Arrayiv.java
@@ -0,0 +1,30 @@
+public class Arrayiv {
+    public static void  main (String args[]){
+        int array[] = new int[]{8, 20, 84, 12, 57, 90};
+
+        int max = getMax(array);
+        System.out.println("Maximum Value is: "+max);
+
+
+        int min = getMin(array);
+        System.out.println("Minimum Value is: "+min);
+    }
+    public static int getMax(int[] inputArray){
+        int maxValue = inputArray[0];
+        for(int i=1;i < inputArray.length;i++){
+            if(inputArray[i] > maxValue){
+                maxValue = inputArray[i];
+            }
+        }
+        return maxValue;
+    }
+    public static int getMin(int[] inputArray){
+        int minValue = inputArray[0];
+        for(int i=1;i<inputArray.length;i++){
+            if(inputArray[i] < minValue){
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
+    }
+}

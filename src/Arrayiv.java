7087eca7f68da8c085e7d12ffda3f1be2c6908f9
diwyanjalee <charithadiwyanjalee@gmail.com>
2021-08-14 15:55:05 +0530

    array non static within static

--- src/Arrayiv.java
+++ src/Arrayiv.java
@@ -1,13 +1,14 @@
 public class Arrayiv {
     public static void  main (String args[]){
         int array[] = new int[]{8, 20, 84, 12, 57, 90};
+        Arrayiv mi = new Arrayiv();
+        mi.getMin(array);
 
         int max = getMax(array);
         System.out.println("Maximum Value is: "+max);
 
-
-        int min = getMin(array);
-        System.out.println("Minimum Value is: "+min);
+        //int min = getMin(array);
+        System.out.println("Minimum Value is: "+mi);
     }
     public static int getMax(int[] inputArray){
         int maxValue = inputArray[0];
@@ -18,12 +19,13 @@ public class Arrayiv {
         }
         return maxValue;
     }
-    public static int getMin(int[] inputArray){
+    public  int getMin(int[] inputArray){
         int minValue = inputArray[0];
         for(int i=1;i<inputArray.length;i++){
             if(inputArray[i] < minValue){
                 minValue = inputArray[i];
             }
+            
         }
         return minValue;
     }

2067911bd4723eab07c0cf9f370d0e470d0f69d3
diwyanjalee <charithadiwyanjalee@gmail.com>
2021-08-25 21:43:50 +0530

    linked list

--- src/Linkedi.java
+++ src/Linkedi.java
@@ -7,11 +7,26 @@ public class Linkedi {
         list.add("cat");
         list.add("dog");
         list.add("elephant");
+        System.out.println("Animal List: " +list);
 
-        Iterator<String> iterator=list.iterator();
+        list.addFirst("owl");
+        list.addLast("fox");
+        list.add(4,"monkey");
+        System.out.println("Animal List after adding animals: " +list);
+
+        list.set(4, "Ape");
+        System.out.println("Animal List after changing index4 animal: " +list);
+
+        list.removeFirst();
+        list.removeLast();
+        list.remove(3);
+        System.out.println("Animal List after removing animals: " +list);
+
+
+        /*Iterator<String> iterator=list.iterator();
         while(iterator.hasNext()){
             System.out.println(iterator.next());
-        }
+        }*/
     }
 
 }
